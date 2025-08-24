472da2e97abdf0366b4a72dd91c077fef25e8f60
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-25 02:12:53 +0900

    chore: add history.ts

---
+++ src/history.ts
@@ -0,0 +1,19 @@
+import type { HistoryProps } from "./routes/Root/Contents/History/history";
+
+//* History Information *//
+// small index (New Information) <-- historyArray --> large index (Old Information)
+export const historyArray: HistoryProps[] = [
+  {
+    imagePath: "/history/establish.webp",
+    title: "きりのはのべる 発足",
+    description: "きりのはのべるが発足しました。",
+    date: "2023-01-01",
+  },
+  {
+    imagePath: "/history/natsuiro.webp",
+    title: "なついろにっき。",
+    description:
+      "きりのはのべるの最初の作品「なついろにっき。」を公開しました。",
+    date: "2023-01-01",
+  },
+];
