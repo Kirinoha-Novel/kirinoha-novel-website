16930db4690a483632b2dadc88b2c36abd1e20cb
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-24 21:47:47 +0900

    chore: update Footer

--- src/routes/Root/Contents/Footer/Footer.tsx
+++ src/routes/Root/Contents/Footer/Footer.tsx
@@ -1,6 +1,15 @@
-import { type FC } from "react";
+import { useEffect, useState, type FC } from "react";
 import styles from "./Footer.module.css";
 
 export const Footer: FC = () => {
-  return <div className={styles.footer}></div>;
+  const [year, setYear] = useState(new Date().getFullYear());
+  useEffect(() => {
+    setYear(new Date().getFullYear());
+  }, []);
+
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copyRight}>&copy; {year} きりのはのべる</p>
+    </footer>
+  );
 };
