b34f17efedd6383bbc76b58b2acbe5a27a65ae63
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-24 17:52:03 +0900

    chore: update FirstView section

--- src/routes/Root/Contents/FirstView/FirstView.tsx
+++ src/routes/Root/Contents/FirstView/FirstView.tsx
@@ -1,13 +1,33 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef, type FC, type RefObject } from "react";
 import styles from "./FirstView.module.css";
-import type { FC } from "react";
 
-const FirstView: FC = () => {
+type Props = {
+  contentsRef: RefObject<HTMLDivElement | null>;
+};
+
+const FirstView: FC<Props> = (props: Props) => {
+  const catchPhrase = useRef<HTMLHeadingElement | null>(null);
+
+  useGSAP(
+    () => {
+      gsap.to(catchPhrase.current, {
+        opacity: 1,
+        duration: 2,
+      });
+    },
+    { scope: props.contentsRef }
+  );
+
   return (
     <div className={styles.firstView}>
       <div className={styles.keyVisualWrapper}>
         <div>
           <div className={styles.catchPhraseWrapper}>
-            <h2 className={styles.catchPhrase}>「きりのは」が綴る物語</h2>
+            <h2 className={styles.catchPhrase} ref={catchPhrase}>
+              「きりのは」が綴る物語
+            </h2>
           </div>
           <img className={styles.keyVisual} src="/leaf.png" alt="leaf" />
         </div>
