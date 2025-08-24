9b5b1aaa60b763fff6c5a885737244e0855cb731
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-24 19:49:41 +0900

    chore: update Contents component

--- src/routes/Root/Contents/Contents.tsx
+++ src/routes/Root/Contents/Contents.tsx
@@ -3,6 +3,7 @@ import styles from "./Contents.module.css";
 import About from "./About/About";
 import FirstView from "./FirstView/FirstView";
 import { News } from "./News/News";
+import { Footer } from "./Footer/Footer";
 
 export const Contents: FC = () => {
   const contents = useRef<HTMLDivElement | null>(null);
@@ -10,8 +11,9 @@ export const Contents: FC = () => {
   return (
     <div className={styles.contents} ref={contents}>
       <FirstView contentsRef={contents} />
-      <News />
       <About contentsRef={contents} />
+      <News contentsRef={contents} />
+      <Footer />
     </div>
   );
 };
