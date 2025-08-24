64fa903e30708283573a2cdf7d922ce3aea67023
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-24 17:52:24 +0900

    chore: update Contents components

--- src/routes/Root/Contents/Contents.tsx
+++ src/routes/Root/Contents/Contents.tsx
@@ -2,13 +2,15 @@ import { useRef, type FC } from "react";
 import styles from "./Contents.module.css";
 import About from "./About/About";
 import FirstView from "./FirstView/FirstView";
+import { News } from "./News/News";
 
 export const Contents: FC = () => {
   const contents = useRef<HTMLDivElement | null>(null);
 
   return (
     <div className={styles.contents} ref={contents}>
-      <FirstView />
+      <FirstView contentsRef={contents} />
+      <News />
       <About contentsRef={contents} />
     </div>
   );
