a39050e6583af170db74063716b11fe7775db40e
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-25 00:32:52 +0900

    chore: update Title

---
+++ src/routes/Root/Contents/Title/Title.tsx
@@ -0,0 +1,36 @@
+import { useRef, type FC, type RefObject } from "react";
+import styles from "./Title.module.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+type Props = {
+  title: string;
+  contentsRef: RefObject<HTMLDivElement | null>;
+};
+
+export const Title: FC<Props> = (props: Props) => {
+  const title = useRef<HTMLHeadingElement | null>(null);
+
+  useGSAP(
+    () => {
+      gsap.to(title.current, {
+        opacity: 1,
+        y: -20,
+        duration: 1,
+        scrollTrigger: {
+          scroller: props.contentsRef.current,
+          trigger: title.current,
+          start: "top center",
+          markers: true,
+        },
+      });
+    },
+    { scope: props.contentsRef }
+  );
+
+  return (
+    <h1 className={styles.title} ref={title}>
+      {props.title}
+    </h1>
+  );
+};
