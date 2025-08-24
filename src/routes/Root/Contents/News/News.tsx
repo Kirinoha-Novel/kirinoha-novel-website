38dde00928994d578275a77d48b07468f0a30cc7
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-24 17:52:10 +0900

    feat: add News section

---
+++ src/routes/Root/Contents/News/News.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+import styles from "./News.module.css";
+
+export const News: FC = () => {
+  return (
+    <section>
+      <h2 className={styles.title}>News</h2>
+    </section>
+  );
+};
