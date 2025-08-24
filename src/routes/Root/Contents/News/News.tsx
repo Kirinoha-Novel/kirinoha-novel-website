477c8c40b0ddad9344d07b14c76abdda45a02cec
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-24 19:48:35 +0900

    chore: update News

--- src/routes/Root/Contents/News/News.tsx
+++ src/routes/Root/Contents/News/News.tsx
@@ -1,10 +1,69 @@
-import type { FC } from "react";
+import { useRef, type FC, type RefObject } from "react";
 import styles from "./News.module.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
-export const News: FC = () => {
+type Props = {
+  contentsRef: RefObject<HTMLDivElement | null>;
+};
+
+export const News: FC<Props> = (props: Props) => {
+  const title = useRef<HTMLHeadingElement | null>(null);
+  const twitterWrapper = useRef<HTMLAnchorElement | null>(null);
+
+  useGSAP(
+    () => {
+      gsap.to(title.current, {
+        opacity: 1,
+        y: -20,
+        duration: 1,
+        scrollTrigger: {
+          scroller: props.contentsRef.current,
+          trigger: title.current,
+          start: "top center",
+          markers: true,
+        },
+      });
+
+      gsap.to(twitterWrapper.current, {
+        opacity: 1,
+        y: -20,
+        duration: 1,
+        scrollTrigger: {
+          scroller: props.contentsRef.current,
+          trigger: twitterWrapper.current,
+          start: "top center",
+          markers: true,
+        },
+      });
+    },
+    { scope: props.contentsRef }
+  );
+
   return (
     <section>
-      <h2 className={styles.title}>News</h2>
+      <h1 className={styles.title} ref={title}>
+        News
+      </h1>
+      <a
+        href={"https://x.com/kirinohanobel"}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.twitterWrapper}
+        ref={twitterWrapper}
+      >
+        <div className={styles.iconWrapper}>
+          <img className={styles.icon} src="/icon.png" alt="circle icon" />
+        </div>
+        <div>
+          <h2 className={styles.twitterAccount}>
+            アカウント名: @kirinohanobel
+          </h2>
+          <p className={styles.twitterDescription}>
+            情報発信は、X (旧: Twitter) で行っています。
+          </p>
+        </div>
+      </a>
     </section>
   );
 };
