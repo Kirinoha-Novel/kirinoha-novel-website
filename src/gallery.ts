28ea96e2f1b870b0b5c8646a7c4e23101cb1accc
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-25 15:09:17 +0900

    chore: add gallery images

---
+++ src/gallery.ts
@@ -0,0 +1,20 @@
+import type { GalleryProps } from "./routes/Root/Contents/Gallery/gallery";
+
+export const GalleryArray: GalleryProps[] = [
+  {
+    imagePath: "/gallery/natsuiro-key-visual.webp",
+    style: "cover",
+  },
+  {
+    imagePath: "/gallery/natsuiro-logo.webp",
+    style: "contain",
+  },
+  {
+    imagePath: "/gallery/establish.webp",
+    style: "cover",
+  },
+  {
+    imagePath: "/gallery/kirinoha-novel-logo.webp",
+    style: "contain",
+  },
+];
